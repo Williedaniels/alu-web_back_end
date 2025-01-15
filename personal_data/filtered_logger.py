0e77e43bc1df4c2ea1932ec68be793764e826797
Williedaniels <w.daniels@alustudent.com>
2025-01-15 13:39:18 +0200

    Updated Filtere Logger file

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,7 +3,7 @@
 import re
 from typing import List
 import logging
-import mysql.connector # type: ignore
+import mysql.connector  # type: ignore
 from os import getenv
 
 PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
